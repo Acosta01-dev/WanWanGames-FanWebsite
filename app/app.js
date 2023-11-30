1954d549db82c1f98018853e0734976f8e053579
Cristian <cristian.acosta01@yahoo.com>
2023-11-30 17:47:47 -0300

    minor changes in js

--- app/app.js
+++ app/app.js
@@ -2,19 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
     var menuToggle = document.querySelector('.burger-menu');
     var headerNav = document.querySelector('.header_nav');
     var ulElement = document.querySelector('.header_nav ul');
-    var menuLink = document.getElementById('menuLink');
     var menuIcon = document.getElementById('menuIcon');
     var closeIcon = document.getElementById('closeIcon');
-    // Agrega un evento de clic al menú hamburguesa
+
     menuToggle.addEventListener('click', function () {
-        headerNav.classList.toggle('active'); // Agrega o quita la clase 'active'
+        headerNav.classList.toggle('active');
+
+        // Agrega o quita la clase 'active' después de un breve retraso
+        setTimeout(function () {
+            ulElement.style.height = headerNav.classList.contains('active') ? '100%' : '0';
+        }, 0);
+
+
+        // Cambia la visibilidad de los íconos de menú y cerrar con una transición
         menuIcon.style.display = (menuIcon.style.display === 'none') ? '' : 'none';
         closeIcon.style.display = (closeIcon.style.display === 'none') ? '' : 'none';
-        // Cambia la altura de ul de 0 a 100% al agregar/quitar la clase 'active'
-        if (headerNav.classList.contains('active')) {
-            ulElement.style.height = '100%';
-        } else {
-            ulElement.style.height = '0';
-        }
     });
 });
