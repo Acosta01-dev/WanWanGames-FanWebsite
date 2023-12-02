c5d78a9513176d22d7da489ba8ab0a93077ce6d4
Cristian <cristian.acosta01@yahoo.com>
2023-12-02 00:05:48 -0300

    added animation burguer menu

--- app/app.js
+++ app/app.js
@@ -1,21 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var ribbon = document.querySelector('.ribbon');
     var menuToggle = document.querySelector('.burger-menu');
-    var headerNav = document.querySelector('.header_nav');
-    var ulElement = document.querySelector('.header_nav ul');
     var menuIcon = document.getElementById('menuIcon');
     var closeIcon = document.getElementById('closeIcon');
 
     menuToggle.addEventListener('click', function () {
-        headerNav.classList.toggle('active');
-
-        // Agrega o quita la clase 'active' después de un breve retraso
-        setTimeout(function () {
-            ulElement.style.height = headerNav.classList.contains('active') ? '100%' : '0';
-        }, 0);
-
 
         // Cambia la visibilidad de los íconos de menú y cerrar con una transición
         menuIcon.style.display = (menuIcon.style.display === 'none') ? '' : 'none';
         closeIcon.style.display = (closeIcon.style.display === 'none') ? '' : 'none';
+
+        // Activa o desactiva la clase 'active' en la cinta (ribbon)
+        ribbon.classList.toggle('active');
     });
 });
